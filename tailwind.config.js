978030326a896d610d13a64ce132f3012ba396a1
Luca Patera <lucapatera@outlook.it>
2024-05-10 02:15:41 +0200

    Add Tailwind support to other file paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,10 @@ export default {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.vue',
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.ts',
+        './resources/**/*.vue',
     ],
 
     theme: {
