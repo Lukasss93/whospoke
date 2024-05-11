4050f8c63b614adee4e6dac5a12625ca7b9f40d0
Luca Patera <lucapatera@outlook.it>
2024-05-11 22:19:52 +0200

    Add force https support

--- config/app.php
+++ config/app.php
@@ -53,6 +53,7 @@ return [
     */
 
     'url' => env('APP_URL', 'http://localhost'),
+    'https' => env('FORCE_HTTPS', false),
 
     /*
     |--------------------------------------------------------------------------
