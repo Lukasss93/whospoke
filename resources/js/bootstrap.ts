d88dc7c666edf8feb9f6ded931ea61723901402e
Luca Patera <lucapatera@outlook.it>
2024-05-13 23:56:41 +0200

    Add Laravel Reverb + Echo

--- resources/js/bootstrap.ts
+++ resources/js/bootstrap.ts
@@ -1,4 +1,12 @@
 import axios from 'axios';
+import './echo';
+
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+/**
+ * Echo exposes an expressive API for subscribing to channels and listening
+ * for events that are broadcast by Laravel. Echo and event broadcasting
+ * allows your team to easily build robust real-time web applications.
+ */
