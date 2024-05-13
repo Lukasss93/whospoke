59893ea7461fb66c81102a4d440faafb345a9bd0
Luca Patera <lucapatera@outlook.it>
2024-05-13 19:13:47 +0200

    Refactor checkbox style

--- resources/js/Components/Checkbox.vue
+++ resources/js/Components/Checkbox.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed } from 'vue';
+import {computed} from 'vue';
 
 const emit = defineEmits(['update:checked']);
 
@@ -24,6 +24,6 @@ const proxyChecked = computed({
         type="checkbox"
         :value="value"
         v-model="proxyChecked"
-        class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800"
+        class="rounded bg-red-300 dark:bg-red-900 border-red-300 dark:border-red-700 text-green-600 shadow-sm focus:ring-green-500 dark:focus:ring-green-600 dark:focus:ring-offset-red-800"
     />
 </template>
