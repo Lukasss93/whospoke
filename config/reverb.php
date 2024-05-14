a4a104213094c54309db8098c4a58201386bcb6d
Luca Patera <lucapatera@outlook.it>
2024-05-15 01:50:14 +0200

    Revert "Add REVERB_CERT env"
    This reverts commit 07cd87c5169084acee2709bd102ef7f3a6c9e4eb.

--- config/reverb.php
+++ config/reverb.php
@@ -33,9 +33,7 @@ return [
             'port' => env('REVERB_SERVER_PORT', 8080),
             'hostname' => env('REVERB_HOST'),
             'options' => [
-                'tls' => [
-                    'local_cert' => env('REVERB_CERT'),
-                ],
+                'tls' => [],
             ],
             'max_request_size' => env('REVERB_MAX_REQUEST_SIZE', 10_000),
             'scaling' => [
