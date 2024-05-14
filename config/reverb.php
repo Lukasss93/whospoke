07cd87c5169084acee2709bd102ef7f3a6c9e4eb
Luca Patera <lucapatera@outlook.it>
2024-05-14 22:13:53 +0200

    Add REVERB_CERT env

--- config/reverb.php
+++ config/reverb.php
@@ -33,7 +33,9 @@ return [
             'port' => env('REVERB_SERVER_PORT', 8080),
             'hostname' => env('REVERB_HOST'),
             'options' => [
-                'tls' => [],
+                'tls' => [
+                    'local_cert' => env('REVERB_CERT'),
+                ],
             ],
             'max_request_size' => env('REVERB_MAX_REQUEST_SIZE', 10_000),
             'scaling' => [
