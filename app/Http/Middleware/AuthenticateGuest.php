cbe19d0c912070d2a2d464fca2343196f0008921
Luca Patera <lucapatera@outlook.it>
2024-05-23 00:44:28 +0200

    Add better guest names

--- app/Http/Middleware/AuthenticateGuest.php
+++ app/Http/Middleware/AuthenticateGuest.php
@@ -7,17 +7,16 @@ use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
+use Nubs\RandomNameGenerator\Alliteration;
 
 class AuthenticateGuest
 {
     public function handle(Request $request, Closure $next)
     {
         if (!Auth::check()) {
-            $id = hrtime(true);
-
             $user = User::make([
-                'id' => -$id,
-                'first_name' => 'Guest ' . $id,
+                'id' => -hrtime(true),
+                'first_name' => (new Alliteration())->getName(),
             ]);
 
             Auth::login($user);
