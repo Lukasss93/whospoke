8ac446301fe90203d44aeccc15dbc3dab6e0dda5
Luca Patera <lucapatera@outlook.it>
2024-06-06 00:14:51 +0200

    Add support for pusher

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -5,11 +5,12 @@ import Pusher from 'pusher-js';
 window.Pusher = Pusher;
 
 window.Echo = new Echo({
-    broadcaster: 'reverb',
-    key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
-    wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
-    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+    broadcaster: import.meta.env.VITE_BROADCASTER,
+    cluster: import.meta.env.VITE_BROADCAST_CLUSTER,
+    key: import.meta.env.VITE_BROADCAST_KEY,
+    wsHost: import.meta.env.VITE_BROADCAST_HOST,
+    wsPort: import.meta.env.VITE_BROADCAST_PORT ?? 80,
+    wssPort: import.meta.env.VITE_BROADCAST_PORT ?? 443,
+    forceTLS: (import.meta.env.VITE_BROADCAST_SCHEME ?? 'https') === 'https',
     enabledTransports: ['ws', 'wss'],
 });
