431f593f6ee5054a23eea91fa6058a1d774da49a
Luca Patera <lucapatera@outlook.it>
2024-07-10 01:13:41 +0200

    Set member type from pending to default when the linked user joins the room

--- routes/channels.php
+++ routes/channels.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Enums\MemberType;
+use App\Events\RoomChangedEvent;
 use App\Http\Middleware\AuthenticateGuest;
+use App\Models\Room;
 use App\Models\User;
 use Illuminate\Support\Facades\Broadcast;
 
@@ -10,6 +13,21 @@ Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
 
 Broadcast::routes(['middleware' => ['web', AuthenticateGuest::class]]);
 
-Broadcast::channel('room.{id}.online', function (User $user) {
+Broadcast::channel('room.{id}.online', function (User $user, $roomId) {
+    $room = Room::find($roomId);
+
+    $linkedMember = $room->members()
+        ->where('user_id', $user->id)
+        ->where('type', MemberType::PENDING)
+        ->first();
+
+    if ($linkedMember !== null) {
+        $linkedMember->type = MemberType::DEFAULT;
+        $linkedMember->save();
+
+        $room->refresh();
+        RoomChangedEvent::dispatch($room);
+    }
+
     return $user;
 });
